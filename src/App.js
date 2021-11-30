e7c528d0bd21c8eb9be008e749df893cfe856ad4
Edward Paler <paler.edward@gmail.com>
2021-11-30 15:58:55 +0800

    "Add Basic Operands"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
+import "./styles.css"
+import DigitButton from './DigitButton'
+import OperationButton from './OperationButton'
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, { type, payload }) {
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      if(payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      }
+      if(payload.digit === "." && state.currentOperand.includes(".")) {
+        return state
+      }
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      }
+      case ACTIONS.CHOOSE_OPERATION:
+        if (state.currentOperand == null && state.previousOperand == null) {
+          return state
+        }
+
+        if(state.previousOperand == null) {
+          return {
+            ...state,
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand: null,
+          }
+        }
+
+        return {
+          ...state, 
+          previousOperand: evaluate(state),
+          operation: payload.operation,
+          currentOperand: null
+        }
+    case ACTIONS.CLEAR:
+       return {}
+    
+    default: return
+  }
+}
+
 
 function App() {
+
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{ previousOperand }  {operation }</div>
+        <div className="current-operand">{ currentOperand }</div>        
+      </div>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button>
+      <button>DEL</button>
+      <OperationButton operation="÷" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <button className="span-two">=</button>
     </div>
-  );
+  )
 }
 
 export default App;
